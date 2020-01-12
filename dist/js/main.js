865daf20b1ea3983497f650368a1ae449480105c
Madhu Dutta <mdutta787@gmail.com>
2020-01-13 06:03:44 +1100

    Few adjustments and design mods in about file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,10 @@
-// alert ('hi');
-//Select DOM objects
+//alert ('hi');
+//Select DOM objects onclick
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-
 
 //Set Initial State of Menu
 let showMenu = false;
